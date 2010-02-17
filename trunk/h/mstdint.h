eedc4d1095704fcf0edb967522fef65be9704679
necto.ne <necto.ne@0b8b07ca-f9fe-11de-aabd-2bfef65f77b5>
2010-02-17 19:41:42 +0000

    missed files

---
+++ trunk/h/mstdint.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   mstdint.h
+ * Author: necto
+ *
+ * Created on February 17, 2010, 9:21 PM
+ */
+
+#ifndef _MSTDINT_H
+#define	_MSTDINT_H
+#include <stdint.h>
+
+#ifndef _SDL_H
+typedef int8_t		Sint8;
+typedef uint8_t		Uint8;
+typedef int16_t		Sint16;
+typedef uint16_t	Uint16;
+typedef int32_t		Sint32;
+typedef uint32_t	Uint32;
+#endif
+
+
+#endif	/* _MSTDINT_H */
+
